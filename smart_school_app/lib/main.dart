541b3569b91ded27071b06f25b0b0ac84d68ea6b
Ghada Ahmed <ga2513092@gmail.com>
2025-10-22 17:43:40 +0300

    resolve Gradle and Firebase build issues

--- smart_school_app/lib/main.dart
+++ smart_school_app/lib/main.dart
@@ -1,6 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:eschool/app/app.dart';
 
-///v-3.2.1
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
   await initializeApp();
 }
